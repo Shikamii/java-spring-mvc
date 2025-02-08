537772b036c26a08d823354f42b442592a3415e4
DUCNA1 <nguyenanhduchatake2003@gmail.com>
2025-02-09 00:07:06 +0700

    init

---
+++ src/main/java/com/ducna1/learnspring/helloController.java
@@ -0,0 +1,20 @@
+package com.ducna1.learnspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class helloController {
+  @GetMapping("/")
+  public String index() {
+      return "Hello, DucN";
+  }
+  @GetMapping("/user")
+  public String userPage() {
+      return "Only user can assess this page";
+  }
+  @GetMapping("/admin")
+  public String adminPage() {
+      return "Only admin can assess this page";
+  }
+}
---
+++ src/test/java/com/ducna1/learnspring/LearnspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducna1.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
