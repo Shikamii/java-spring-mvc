e034656f38bf0ac1aa4e0f10016d0ffcdffc0da5
ducna1 <ducna1@hblab.vn>
2025-02-11 16:01:04 +0700

    truyen data tu controller qua view, jsp, jstl

--- src/main/java/com/ducna1/learnspring/controller/UserController.java
+++ src/main/java/com/ducna1/learnspring/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ducna1.learnspring.controller;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,11 @@ public class UserController {
   }
 
   @RequestMapping("/")
-  public String getHomePage() {
+  public String getHomePage(Model model) {
     String test = this.userService.handleHello();
-    return "eric.html";
+    model.addAttribute("eric", test);
+    model.addAttribute("name", "DucNA1 from Hblab");
+    return "hello";
   }
 }
 // @RestController
