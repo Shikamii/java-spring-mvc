508a58be4f9a058cee30d871dfc4edfaa51940ea
ducna1 <ducna1@hblab.vn>
2025-02-11 16:27:48 +0700

    tich hop boostrap va jsquery video 50

--- src/main/java/com/ducna1/learnspring/config/WebMvcConfig.java
+++ src/main/java/com/ducna1/learnspring/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -11,8 +12,11 @@ import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+// @EnableWebMvc: sử dụng để cấu hình cho việc sử dụng Spring MVC
+// class WebMvcConfig implements WebMvcConfigurer: implements WebMvcConfigurer
+// để
+// cấu hình cho việc sử dụng Spring MVC
 public class WebMvcConfig implements WebMvcConfigurer {
-
   @Bean
   public ViewResolver viewResolver() {
     final InternalResourceViewResolver bean = new InternalResourceViewResolver();
@@ -25,5 +29,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
   @Override
   public void configureViewResolvers(ViewResolverRegistry registry) {
     registry.viewResolver(viewResolver());
+  }
+
+  @Override // ghi đè lại phương thức addResourceHandlers để cấu hình cho việc tìm kiếm các
+            // resource (css, js, images, etc...)
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
   }
 }
