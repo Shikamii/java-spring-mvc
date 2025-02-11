e523f39f93707e92c5f83ddc67d92ec333cd076a
ducna1 <ducna1@hblab.vn>
2025-02-11 09:24:40 +0700

    config password and disable spring security video 43 44

--- src/main/java/com/ducna1/learnspring/LearnspringApplication.java
+++ src/main/java/com/ducna1/learnspring/LearnspringApplication.java
@@ -3,9 +3,9 @@ package com.ducna1.learnspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// @SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LearnspringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LearnspringApplication.class, args);
 	}
