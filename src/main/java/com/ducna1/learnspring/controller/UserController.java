da095dce0a7abde868a96334a2d9eb3c337eff46
ducna1 <ducna1@hblab.vn>
2025-02-11 11:28:02 +0700

    render view html video 46

--- src/main/java/com/ducna1/learnspring/controller/UserController.java
+++ src/main/java/com/ducna1/learnspring/controller/UserController.java
@@ -8,29 +8,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ducna1.learnspring.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//   @RequestMapping("/")
-//   public String getHomePage() {
-//     return "hello from controller";
-//   }
-// }
-@RestController
+@Controller
 public class UserController {
-
   private UserService userService;
 
-  // hàm khởi tạo của UserController với tham số là userService (được Spring Boot
-  // tự động inject vào)
-  // và gán cho userService của UserController bằng userService được inject vào đó
   public UserController(UserService userService) {
     this.userService = userService;
   }
 
-  @GetMapping("/")
+  @RequestMapping("/")
   public String getHomePage() {
-    return this.userService.handleHello();
+    String test = this.userService.handleHello();
+    return "eric.html";
   }
-
 }
+// @RestController
+// public class UserController {
+
+// private UserService userService;
+
+// // hàm khởi tạo của UserController với tham số là userService (được Spring
+// Boot
+// // tự động inject vào)
+// // và gán cho userService của UserController bằng userService được inject vào
+// đó
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+
+// }
